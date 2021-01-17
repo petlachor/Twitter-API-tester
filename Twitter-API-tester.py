e9fd81255af7be8a9bb1b776c56e96797ed2a8a0
Petla <66150085+Petla-reboot@users.noreply.github.com>
2021-01-17 12:44:54 +0900

    Update Twitter-API-tester.py

--- Twitter-API-tester.py
+++ Twitter-API-tester.py
@@ -2,19 +2,40 @@ import tweepy
 
 print("*** Twitter API tester ***")
 
-print("Please enter the Consumer Key")
-CK = input("CK >>")
-print("Please enter the Consumer Secret")
-CS = input("CS >>")
-print("Please enter the Access Token")
-AT = input("AT >>")
-print("Please enter the Access Secret")
-AS = input("AS >>")
+while True:
+	try:
+		print("Please enter the Consumer Key")
+		CK = input(">>")
+		if CK == "exit":
+			break
+			sys.exit()
+		print("Please enter the Consumer Secret")
+		CS = input(">>")
+		if CS == "exit":
+			break
+			sys.exit()
+		print("Please enter the Access Token")
+		AT = input(">>")
+		if AT == "exit":
+			break
+			sys.exit()
+		print("Please enter the Access Secret")
+		AS = input(">>")
+		if AS == "exit":
+			break
+			sys.exit()
 
-auth = tweepy.OAuthHandler(CK, CS)
-auth.set_access_token(AT, AS)
-api = tweepy.API(auth)
+		auth = tweepy.OAuthHandler(CK, CS)
+		auth.set_access_token(AT, AS)
+		api = tweepy.API(auth)
 
-print("Please enter the content to tweet")
-tweet = input("Tweet >>")
-api.update_status(tweet)
+		print("Please enter the content to tweet")
+		tweet = input(">>")
+		api.update_status(tweet)
+		
+	except:
+		print("An error has occurred! Please try again.")
+		print("Tip: Type [exit] to exit the program.")
+		
+	else:
+		break
